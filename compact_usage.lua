a475ea93e236d00047678dd6d811306b421f5653
Serophots <tomtidbury07@gmail.com>
2022-07-07 19:55:18 +0100

    Example usage file

--- compact_usage.lua
+++ compact_usage.lua
@@ -1,10 +1,42 @@
-local UI = loadstring(game:HttpGet("http://127.0.0.1:5555/uilib_compact.lua"))()
-UI = UI.init("Showcase", "v1.0.0", "Your Name Here", "SHOWCASE")
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Serophots/my-uilib/main/uilib_compact.lua"))()
+UI = UI.init("Showcase", "v1.0.0", "SHOWCASE")
 
-local TabAim = UI:AddTab("Aim", "Silent Aim") do
-    local SectionSilentAim = TabAim:AddSection("Silent Aim") do
-		SectionSilentAim:AddButton({
-			title = "button",
+local AimOne, AimTwo = UI:AddTab("Aim", "Silent Aim") do
+
+	local Section = AimOne:AddSeperator("Silent Aim") do
+		Section:AddToggle({
+			title = "Enabled"
 		})
-    end
+		Section:AddToggle({
+			title = "Display field of view",
+			checked = true
+		})
+		Section:AddSlider({
+			title = "Field of view",
+			values = {min=0,max=250,default=50}
+		})
+		Section:AddToggle({
+		    title = "Display dead field of view"
+		})
+		Section:AddSlider({
+		    title = "Dead field of view"
+		})
+		Section:AddSelection({
+			title = "Bodyparts",
+			options = {
+			    ["Head"] = "head",
+			    ["Torso"] = "torso",
+			    ["Left Arm"] = "larm",
+			    ["Right Arm"] = "rarm",
+			    ["Left Leg"] = "lleg",
+			    ["Right Arm"] = "rarm",
+			}
+		})
+	end
+	local Section = AimOne:AddSeperator("Tuning") do
+	    Section:AddToggle({
+	        title = "Do tuning"
+	    })
+	end
 end
+local TabTwo = UI:AddTab("Second tab", "some other things")
